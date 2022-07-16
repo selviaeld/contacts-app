346ce2235fddac605ba804ba33e8e4f7d2cd4562
selviaeld <selviaeldina12@gmail.com>
2022-07-16 12:21:49 +0700

    feat(contact): implement unidirectional data flow

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+    return [
+        {
+            id: 1,
+            name: 'Hamada Asahi',
+            tag: 'hmdsahi',
+            imageUrl: '/images/hmdsahi.jpeg',
+        },
+        {
+            id: 2,
+            name: 'Kim Junkyu',
+            tag: 'junkyu',
+            imageUrl: '/images/junkyu.jpeg',
+        },
+        {
+            id: 3,
+            name: 'Choi Hyunsuk',
+            tag: 'hyunsuk',
+            imageUrl: '/images/hyunsuk.jpeg',
+        }
+    ]
+}
+
+export { getData };
